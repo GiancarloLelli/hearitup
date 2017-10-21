ffbb94bd0a65ce3111276c64be1f63efe27dce8e
Giancarlo Lelli <gcarlo.lelli@live.com>
2017-10-21 15:59:14 +0200

    App retarget to FCU

--- HIU.Data/Migrations/MusicRecordContextModelSnapshot.cs
+++ HIU.Data/Migrations/MusicRecordContextModelSnapshot.cs
@@ -5,45 +5,45 @@ using System;
 
 namespace HIU.Data.Migrations
 {
-    [DbContext(typeof(MusicRecordContext))]
-    partial class MusicRecordContextModelSnapshot : ModelSnapshot
-    {
-        protected override void BuildModel(ModelBuilder modelBuilder)
-        {
-            try
-            {
-                modelBuilder.HasAnnotation("ProductVersion", "1.0.0-rtm-21431");
-                modelBuilder.Entity("HIU.Model.Repository.MusicRecordItem", b =>
-                {
-                    b.Property<int>("Id").ValueGeneratedOnAdd();
+	[DbContext(typeof(MusicRecordContext))]
+	partial class MusicRecordContextModelSnapshot : ModelSnapshot
+	{
+		protected override void BuildModel(ModelBuilder modelBuilder)
+		{
+			try
+			{
+				modelBuilder.HasAnnotation("ProductVersion", "2.0.0");
+				modelBuilder.Entity("HIU.Model.Repository.MusicRecordItem", b =>
+				{
+					b.Property<int>("Id").ValueGeneratedOnAdd();
 
-                    // Root object
-                    b.Property<string>("MicrosoftId");
-                    b.Property<string>("Domain");
-                    b.Property<string>("Type");
-                    b.Property<string>("Url");
-                    b.Property<string>("SecondsOffset");
-                    b.Property<DateTime>("DiscoveredOn");
-                    b.Property<bool>("IsNull");
-                    b.Property<string>("ImageUrl");
-                    b.Property<string>("SpotifyId");
-                    b.Property<bool>("AddedToSpotify");
+					// Root object
+					b.Property<string>("MicrosoftId");
+					b.Property<string>("Domain");
+					b.Property<string>("Type");
+					b.Property<string>("Url");
+					b.Property<string>("SecondsOffset");
+					b.Property<DateTime>("DiscoveredOn");
+					b.Property<bool>("IsNull");
+					b.Property<string>("ImageUrl");
+					b.Property<string>("SpotifyId");
+					b.Property<bool>("AddedToSpotify");
 
-                    // Properties
-                    b.Property<string>("SongType");
-                    b.Property<string>("SongTitle");
-                    b.Property<string>("ArtistName");
-                    b.Property<string>("ReleaseTitle");
-                    b.Property<string>("AudioPreview");
+					// Properties
+					b.Property<string>("SongType");
+					b.Property<string>("SongTitle");
+					b.Property<string>("ArtistName");
+					b.Property<string>("ReleaseTitle");
+					b.Property<string>("AudioPreview");
 
-                    b.HasKey("Id");
-                    b.ForSqliteToTable("Records");
-                });
-            }
-            catch (Exception)
-            {
-                throw;
-            }
-        }
-    }
+					b.HasKey("Id");
+					b.ToTable("Records");
+				});
+			}
+			catch (Exception)
+			{
+				throw;
+			}
+		}
+	}
 }
